8899b006255040dd99858796c38161c78c3f5894
张哲珲 <zzh19971213@163.com>
2020-10-28 09:27:33 +0800

    服务端消息处理

---
+++ src/main/java/com/smartcat/rpc/netty/NettyServerHandler.java
@@ -0,0 +1,45 @@
+package com.smartcat.rpc.netty;
+
+import com.smartcat.rpc.kryo.dto.RpcRequest;
+import com.smartcat.rpc.kryo.dto.RpcResponse;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * description: NettyServerHandler
+ * date: 2020/10/28 9:23
+ * 服务端消息处理类
+ * @author: 张哲珲
+ * version: 1.0.0
+ */
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(NettyServerHandler.class);
+    private static final AtomicInteger atomicInteger = new AtomicInteger(1);
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        try {
+            RpcRequest rpcRequest = (RpcRequest) msg;
+            logger.info("server receive msg: [{}] ,times:[{}]", rpcRequest, atomicInteger.getAndIncrement());
+            RpcResponse messageFromServer = RpcResponse.builder().message("message from server").build();
+            ChannelFuture f = ctx.writeAndFlush(messageFromServer);
+            f.addListener(ChannelFutureListener.CLOSE);
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        logger.error("server catch exception",cause);
+        ctx.close();
+    }
+}
