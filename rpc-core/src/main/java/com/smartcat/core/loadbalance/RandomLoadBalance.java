5eb09139a0713cdfd78e81af2cd3693075e8443b
SmartCat994 <zzh19971213@163.com>
2020-11-04 09:11:12 +0800

    局部优化

--- rpc-core/src/main/java/com/smartcat/core/loadbalance/RandomLoadBalance.java
+++ rpc-core/src/main/java/com/smartcat/core/loadbalance/RandomLoadBalance.java
@@ -11,16 +11,15 @@ import java.util.Random;
  * version: 1.0.0
  */
 public class RandomLoadBalance extends AbstractLoadBalance {
+    Random random = new Random();
 
     /**
      * 随机返回一个地址
-     * 这边秀了一下技巧,其实不用抽象类的,单纯的大脑抽了
      * @param serviceAddresses 服务的地址
      * @return 服务地址
      */
     @Override
     protected String doSelect(List<String> serviceAddresses) {
-        Random random = new Random();
         return serviceAddresses.get(random.nextInt(serviceAddresses.size()));
     }
 }
