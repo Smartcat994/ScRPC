705c1d19fdbcfe6eb33376a161657711a8af6038
SmartCat994 <zzh19971213@163.com>
2020-11-12 15:51:06 +0800

    拓展加载类

---
+++ rpc-common/src/main/java/com/smartcat/common/extension/SPI.java
@@ -0,0 +1,17 @@
+package com.smartcat.common.extension;
+
+import java.lang.annotation.*;
+
+/**
+ * description: SPI
+ * date: 2020/11/12 15:32
+ *
+ * @author: SmartCat
+ * version: 1.0.0
+ */
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface SPI {
+}
